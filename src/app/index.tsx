9ffc993cb338af98e2964fb62cb82eada0eed1fe
ahmedabelrahman-dev <rman79340@gmail.com>
2026-03-16 07:21:20 +0400

    setup NativeWind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello There !</Text>
+      <Text className="text-red-500">Hello There !</Text>
       <Image
         source={require('../../assets/images/icon.png')}
         style={{ height: 100, width: 100 }}
